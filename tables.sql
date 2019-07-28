60e114ab83fbe3dafbdb6359702d885343533b31
Lennard Gorter <lennardgorter@10-197-36-145.sds.oregonstate.edu>
2019-07-27 23:42:39 -0700

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,5 +1,6 @@
 USE cs340_gorterl;
 
+ALTER TABLE `team` DISABLE KEYS;
 DROP TABLE IF EXISTS `stadium`;
 CREATE TABLE `stadium` (
     `stadiumId` int NOT NULL AUTO_INCREMENT,
@@ -10,6 +11,7 @@ CREATE TABLE `stadium` (
     `year` int,
     PRIMARY KEY (`stadiumId`)
 );
+ALTER TABLE `team` ENABLE KEYS;
 
 INSERT INTO `stadium` VALUES (1, 'Etihad Stadium', 'Manchester, England', 55097, 'GrassMaster', 2002),
 (2, 'Anfield', 'Liverpool, England', 54074, 'GrassMaster', 1884),
