e138d7ce1fd0a4357b9cfe5e4445b462b49a415b
Lennard Gorter <lennardgorter@10-197-32-33.sds.oregonstate.edu>
2019-08-08 20:34:00 -0700

    Update dmq.sql

--- dmq.sql
+++ dmq.sql
@@ -6,45 +6,45 @@ NOTE: The identifier for backend variables is the colon, :
 */
 
 /* Select all Teams for the Team drop-down */
-SELECT teamId, name FROM Team
+SELECT teamId, name FROM Team;
 
 /* Select all Stadiums for the Stadium drop-down */
-SELECT stadiumId, name FROM Stadium
+SELECT stadiumId, name FROM Stadium;
 
 /* Select all Players for the Player drop-down */
-SELECT playerId, name FROM Player
+SELECT playerId, name FROM Player;
 
 /* Select all Leagues for the League drop-down */
-SELECT leagueId, name FROM League
+SELECT leagueId, name FROM League;
 
 /* Select all Sponsors for the Sponsor drop-down */
-SELECT sponsorId, name FROM Sponsor
+SELECT sponsorId, name FROM Sponsor;
 
 /* Insert into Team table */
-INSERT INTO Team (name, city, stadium, standing, gamesPlayed, points) VALUES (:team_name, :team_city, :team_stadium, :team_standing, :team_gamesPlayed, :points)
+INSERT INTO Team (name, city, stadium, standing, gamesPlayed, points) VALUES (:team_name, :team_city, :team_stadium, :team_standing, :team_gamesPlayed, :points);
 
 /* Insert into Stadium table */
-INSERT INTO Stadium (name, city, capacity, pitch, year) VALUES (:stadium_name, :stadium_city, :stadium_capacity, :stadium_pitch, :stadium_year)
+INSERT INTO Stadium (name, city, capacity, pitch, year) VALUES (:stadium_name, :stadium_city, :stadium_capacity, :stadium_pitch, :stadium_year);
 
 /* Insert into League table */
-INSERT INTO League (name, location, champ, totalGames, year) VALUES (:league_name, :league_location, :league_champion, :league_totalGames, :league_year)
+INSERT INTO League (name, location, champ, totalGames, year) VALUES (:league_name, :league_location, :league_champion, :league_totalGames, :league_year);
 
 /* Insert into Player table */
-INSERT INTO Player (team, fname, lname, position, appearances, goals, assists, height, birthDate, nationality, sponsor) VALUES (:player_team, :player_firstname, :player_lastname, :player_position, :player_appearances, :player_goals, :player_assists, :player_height, :player_birthdate, :player_nationality, :player_sponsorship)
+INSERT INTO Player (team, fname, lname, position, appearances, goals, assists, height, birthDate, nationality, sponsor) VALUES (:player_team, :player_firstname, :player_lastname, :player_position, :player_appearances, :player_goals, :player_assists, :player_height, :player_birthdate, :player_nationality, :player_sponsorship);
 
 /* Insert into Sponsor table */
-INSERT INTO Sponsor (name, revenue) VALUES (:sponsor_name, :sponsor_revenue)
+INSERT INTO Sponsor (name, revenue) VALUES (:sponsor_name, :sponsor_revenue);
 
 /* Update User-chosen table and attribute */
-UPDATE :table SET :attribute = :attribute_value WHERE :table.id = :id_chosen_from_dropdown
+UPDATE :table SET :attribute = :attribute_value WHERE :table.id = :id_chosen_from_dropdown;
 
 /* Delete User-chosen row from table */
-DELETE FROM :table WHERE :table.id = :id_chosen_from_dropdown
+DELETE FROM :table WHERE :table.id = :id_chosen_from_dropdown;
 
 /* Select all Players with 10+ goals that play in England */
 SELECT fname, lname FROM Player 
 WHERE Player.goals >= 10 AND Player.nationality = "England"
-GROUP BY lname
+ORDER BY lname;
 
 /* Display all Teams */
 SELECT * FROM team;
